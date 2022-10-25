085f0ef32b2d6b448316f2762b6013d22d372389
Nibardo Vera Diaz <Lic_utel@hotmail.com>
2022-10-24 22:50:30 -0500

    validacion de datos

--- eventos.js
+++ eventos.js
@@ -1,5 +1,44 @@
 (function(){
-    var formulario=document.getElementById('formulario')
-    elementos=formulario.elements,
+    var formulario=document.getElementsByName('formulario')[0],
+    elementos = formulario.elements,
     boton = document.getElementById('btn')
-})
+// validar nombre 
+    var validarNombre=function(e){
+        if(formulario.nombre.value == 0){
+            alert("Completa el campo nombre");
+            e.preventDefault();
+        }
+    };
+
+    // validar appelido
+    var validarApellido=function(e){
+        if(formulario.apellido.value == 0 ){
+            alert("Completa el campo apellido");
+            e.preventDefault();
+        }
+    }
+// validar sexo
+    var validarRadio=function(e){
+        if(formulario.sexo[0].checked == true || formulario.sexo[1].checked == true){}
+        else{
+            alert("completa el campo sexo");
+            e.preventDefault();
+        }
+    };
+// validar terminos
+    var validarCheckbox = function(e) {
+        if(formulario.terminos.checked == false){
+            alert("Acepta los terminos");
+            e.preventDefault();
+        }
+    }
+
+    var validar=function(e){
+        validarNombre(e);
+        validarApellido(e);
+        validarRadio(e);
+        validarCheckbox(e);
+    };
+ 
+    formulario.addEventListener("submit", validar,);
+}())
